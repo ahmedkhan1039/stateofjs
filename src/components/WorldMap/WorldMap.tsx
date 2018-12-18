64b608cf6e0163d1e509d32e2d9c0a1cf6ec59c5
Muhammad Ali <mr.muhammad.ali@live.com>
2018-12-18 23:22:02 +0500

    refactoring

--- src/components/WorldMap/WorldMap.tsx
+++ src/components/WorldMap/WorldMap.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
 
 // src
-import './styles.css'
+import './WorldMap.css'
 
 type Longitude = number
 type Latitude = number
